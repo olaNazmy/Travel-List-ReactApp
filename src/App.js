3245b48f5aa2bda452277e83084f91c599b9a9cf
ola <olanazmy@gmail.com>
2025-06-21 03:52:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+function Logo() {
+  return <h1>🏝️ Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list, and you already packed X (X%);</em>
+    </footer>
+  );
+}
